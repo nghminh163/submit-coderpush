69ddb6a514e2e2514883052cca22c7e80b08f271
Restyled.io <commits@restyled.io>
2022-04-19 17:36:56 +0000

    Restyled by prettier

--- tinder-app-fe/next.config.js
+++ tinder-app-fe/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode : true,
-  typescript : {
-    ignoreBuildErrors :
-        true, // -> Temporary because typescript of tinder-card lib outdate
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true, // -> Temporary because typescript of tinder-card lib outdate
   },
 };
 
